7523892d66971f39890a584bec24bb594bbffd5f
Şafak Şahin <safaksahin022@gmail.com>
2024-05-26 10:48:51 +0300

    entity layer

---
+++ BlogForest.EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.EntityLayer.Concrete
+{
+	public class Blog
+	{
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string ThumbNailImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int ViewCount { get; set; }
+        public string CoverImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
