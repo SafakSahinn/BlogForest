770f24ab20aa0fad8be2979b1cce9f4b4788e5ff
Şafak Şahin <safaksahin022@gmail.com>
2024-05-27 14:19:09 +0300

    ..

--- BlogForest.WebUI/ViewComponents/BlogViewComponents/_BlogListComponentPartial.cs
+++ BlogForest.WebUI/ViewComponents/BlogViewComponents/_BlogListComponentPartial.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogForest.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogForest.WebUI.ViewComponents.BlogViewComponents
 {
 	public class _BlogListComponentPartial : ViewComponent
 	{
+		private readonly IBlogService _blogService;
+		public _BlogListComponentPartial(IBlogService blogService)
+		{
+			_blogService = blogService;
+		}
+
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			var values = _blogService.TGetListAll();
+			return View(values);
 		}
 	}
 }
