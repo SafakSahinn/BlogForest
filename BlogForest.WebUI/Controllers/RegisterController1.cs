3f1b7defc57d353f9e61addcfc86c619eb0ed643
Şafak Şahin <safaksahin022@gmail.com>
2024-05-27 19:44:46 +0300

    dto

---
+++ BlogForest.WebUI/Controllers/RegisterController1.cs
@@ -0,0 +1,27 @@
+﻿using BlogForest.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogForest.WebUI.Controllers
+{
+	public class RegisterController1 : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public RegisterController1(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpGet]
+		public IActionResult CreateUser()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> CreateUser()
+		{
+
+		}
+	}
+}
